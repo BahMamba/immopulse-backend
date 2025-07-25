5f121689fe6f8302e93c2bc69347d3ddaa4ef973
Bah Mamba <mambafaker224@gmail.com>
2025-07-25 05:33:30 +0000

    revision

--- src/main/java/com/mamba/immopulse_backend/ImmopulseApplication.java
+++ src/main/java/com/mamba/immopulse_backend/ImmopulseApplication.java
@@ -17,26 +17,4 @@ public class ImmopulseApplication {
 		SpringApplication.run(ImmopulseApplication.class, args);
 	}
 
-
-	@Bean
-	public CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		return args -> {
-			String adminEmail = "admin@immopulse.com";
-
-			// Vérifie si l'admin existe déjà
-			if (userRepository.findByEmail(adminEmail).isEmpty()) {
-				User admin = new User();
-				admin.setFullname("Darty Admin");
-				admin.setEmail(adminEmail);
-				admin.setPassword(passwordEncoder.encode("1234")); // Mot de passe hashé
-				admin.setRoleUser(RoleUser.ADMIN);
-				admin.set_active(true);
-
-				userRepository.save(admin);
-				System.out.println("✅ Admin créé avec succès (email: admin@immopulse.com, mot de passe: admin123)");
-			} else {
-				System.out.println("ℹ️ Admin déjà existant. Aucun changement.");
-			}
-		};
-	}
 }
